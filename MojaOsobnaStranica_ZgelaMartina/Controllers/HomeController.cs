83db5f219b786646001becb087896a028a631b2c
gin ger <martina.zgela1@gmail.com>
2019-01-29 00:11:26 +0100

    gotov projekt

---
+++ MojaOsobnaStranica_ZgelaMartina/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MojaOsobnaStranica_ZgelaMartina.Models;
+
+namespace MojaOsobnaStranica_ZgelaMartina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MojaOsobnaStranica_ZgelaMartina/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MojaOsobnaStranica_ZgelaMartina.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
